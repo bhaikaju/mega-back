78c1895bb1f848e7811e5cb2114f9de6524bf5f3
Indranil Mukherjee <bhaikaju@gmail.com>
2020-03-21 19:19:35 +0530

    Creating Order Route

--- routes/orders.js
+++ routes/orders.js
@@ -19,121 +19,118 @@ router.get('/', (req, res) => {
                 on: 'u.id = o.user_id'
             }
         ])
-        .withFields(['o.id', 'p.id', 'p.description', 'p.price', 'p.title as name', 'p.image', 'u.username'])
+        .withFields(['o.id', 'p.title as name', 'p.description', 'p.price', 'u.username'])
+        .sort({id: 1})
         .getAll()
         .then(orders => {
             if (orders.length > 0) {
                 res.status(200).json(orders);
             } else {
-                res.json({message: "No orders found"});
+                res.json({message: 'No orders found'});
             }
-        })
-
-
+        }).catch(err => console.log(err));
 });
 
-/* GET SINGLE ORDER*/
+/* GET SINGLE ORDER */
 router.get('/:id', (req, res) => {
     const orderId = req.params.id;
 
-    if (orderId > 0 && !isNaN(orderId)) {
-        database.table('orders_details as od')
-            .join([
-                {
-                    table: 'orders as o',
-                    on: 'o.id = od.order_id'
-                },
-                {
-                    table: 'products as p',
-                    on: 'p.id = od.product_id'
-                },
-                {
-                    table: 'users as u',
-                    on: 'u.id = o.user_id'
-                }
-            ])
-            .withFields(['o.id', 'p.title as name', 'p.description', 'p.price', 'p.image', 'od.quantity as quantityOrdered'])
-            .filter({'o.id': orderId})
-            .getAll()
-            .then(order => {
-                res.json(order);
-            })
-    }
-});
-
-/* PLACING A NEW ORDER*/
-router.post('/new', async (req, res) => {
-
-  let {userId, products} = req.body;
-
-  if (userId !== null && userId > 0) {
-    database.table('orders')
-        .insert({
-          user_id: userId
-        }).then((newOrderId) => {
-
-      if (newOrderId > 0) {
-        products.forEach(async (p) => {
-
-          let data = await database.table('products').filter({id: p.id}).withFields(['quantity']).get();
-
-
-
-          let inCart = parseInt(p.incart);
-
-          // Deduct the number of pieces ordered from the quantity in database
-
-          if (data.quantity > 0) {
-            data.quantity = data.quantity - inCart;
-
-            if (data.quantity < 0) {
-              data.quantity = 0;
+    database.table('orders_details as od')
+        .join([
+            {
+                table: 'orders as o',
+                on: 'o.id = od.order_id'
+            },
+            {
+                table: 'products as p',
+                on: 'p.id = od.product_id'
+            },
+            {
+                table: 'users as u',
+                on: 'u.id = o.user_id'
             }
+        ])
+        .withFields(['o.id', 'p.title as name', 'p.description', 'p.price', 'u.username'])
+        .filter({'o.id': orderId})
+        .getAll()
+        .then(orders => {
+            if (orders.length > 0) {
+                res.status(200).json(orders);
+            } else {
+                res.json({message: `No orders found with orderId ${orderId}`});
+            }
+        }).catch(err => console.log(err));
+});
 
-          } else {
-            data.quantity = 0;
-          }
+/* PLACE A NEW ORDER*/
+router.post('/new', (req, res) => {
 
-          // Insert order details w.r.t the newly created order Id
-          database.table('orders_details')
-              .insert({
+    let {userId, products} = req.body;
+
+    if (userId !== null && userId > 0 && !isNaN(userId)) {
+        database.table('orders')
+            .insert({
+                user_id: userId
+            }).then(newOrderId => {
+
+            if (newOrderId > 0) {
+                products.forEach(async (p) => {
+
+                    let data = await database.table('products').filter({id: p.id}).withFields(['quantity']).get();
+
+                    let inCart = p.incart;
+
+                    //    Deduct the number of pieces ordered from the quantity column in database
+                    if (data.quantity > 0) {
+                        data.quantity = data.quantity - inCart;
+
+                        if (data.quantity < 0) {
+                            data.quantity = 0;
+                        }
+
+                    } else {
+                        data.quantity = 0;
+                    }
+
+                    //    INSERT ORDER DETAILS W.R.T THE NEWLY GENERATED ORDER ID
+                    database.table('orders_details')
+                        .insert({
+                            order_id: newOrderId,
+                            product_id: p.id,
+                            quantity: inCart
+                        }).then(newId => {
+                        database.table('products')
+                            .filter({id: p.id})
+                            .update({
+                                quantity: data.quantity
+                            }).then(successNum => {
+                        }).catch(err => console.log(err));
+                    }).catch(err => console.log(err));
+
+                });
+            } else {
+                res.json({message: 'new order failed while adding order details', success: false})
+            }
+            res.json({
+                message: `Order successfully placed with order id ${newOrderId}`,
+                success: true,
                 order_id: newOrderId,
-                product_id: p.id,
-                quantity: inCart
-              }).then(newId => {
-            database.table('products')
-                .filter({id: p.id})
-                .update({
-                  quantity: data.quantity
-                }).then(successNum => {
-            }).catch(err => console.log(err));
-          }).catch(err => console.log(err));
-        });
+                products: products
+            });
+        }).catch(err => console.log(err));
+    } else {
+        res.json({message: 'New order failed', success: false});
+    }
 
-      } else {
-        res.json({message: 'New order failed while adding order details', success: false});
-      }
-      res.json({
-        message: `Order successfully placed with order id ${newOrderId}`,
-        success: true,
-        order_id: newOrderId,
-        products: products
-      })
-    }).catch(err => res.json(err));
-  }
-
-  else {
-    res.json({message: 'New order failed', success: false});
-  }
 
 });
 
-/* PAYMENT GATEWAY FAKE REQUEST*/
+
+/* FAKE PAYMENT GATEWAY CALL*/
 router.post('/payment', (req, res) => {
     setTimeout(() => {
-      res.status(200).json({success: true});
-    }, 3000)
+        res.status(200).json({success: true});
+    }, 3000);
 });
-
 
 module.exports = router;
